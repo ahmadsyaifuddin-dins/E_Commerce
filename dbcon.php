dfb5b002413bda5cc102f6e8dac60ba4e3f59d0f
Ahmad Syaifuddin <ahmadsyai598@gmail.com>
2023-11-02 12:32:47 +0800

    Initial commit

---
+++ dbcon.php
@@ -0,0 +1,6 @@
+<?php
+$host = "localhost";
+$tableprefix = "e_commerce_"; //You can change this table prefix, don't use white spaces, use underscores instead for example this_is_my_table_prefix_
+$databasename = "e_commerce";
+$dbuser = "root";
+$dbpassword = "";
